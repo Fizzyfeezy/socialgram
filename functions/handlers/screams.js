54688dbfbe58636f0c41f56cfdf147608a9e42cb
oyelami hafeez <oyelami.abdulhafeez@gmail.com>
2019-09-17 18:13:07 +0100

    update

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -42,3 +42,24 @@ exports.postOneScream = (request, response) => {
         console.error(err);
     })
 }
+
+exports.getScream = (request, response) => {
+    let screamData = {};
+    db.doc(`/screams/${request.params.screamId}`).get().then(doc => {
+        if (!doc.exists) {
+            return response.status(404).json({error : 'Scream not found'})
+        }
+        screamData = doc.data();
+        screamData.screamId = doc.id;
+        return db.collection('comments').where('screamId', '==', request.params.screamId).get().then(data => {
+            screamData.comments = [];
+            data.forEach(doc => {
+                screamData.comments.push(doc.data())
+            })
+            return response.json(screamData)
+        }).catch(err => {
+            console.error(err);
+            return response.status(500).json({error : err.code})
+        })
+    })
+}
--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,22 @@ const app = require('express')();
 // const app = express.Router();
 
 
-const {getAllScreams, postOneScream} = require('./handlers/screams');
-const {signup, login, uploadImage, addUserDetails} = require('./handlers/users');
+const {getAllScreams, postOneScream, getScream} = require('./handlers/screams');
+const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 const {FBAuth} = require('./util/fbAuth');
 
 
 //Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.https.onRequest(app);
